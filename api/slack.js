de77bb7da1c91f80c30d750647cb839c18b4b3a9
Aviancaswat <lauritablanco2495@gmail.com>
2025-08-15 09:47:51 -0500

    cambio de posicion en el console.log

--- api/slack.js
+++ api/slack.js
@@ -1,38 +1,38 @@
 // /api/slack.js
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
-  const { mensaje } = req.body;
-  const githubToken = process.env.GITHUB_TOKEN;
-
-  try {
-    console.log("TOKEN", process.env.GITHUB_TOKEN ? "CARGADO" : "NO CARGADO");
-
-    const response = await fetch(
-      "https://api.github.com/repos/Aviancaswat/avianca-test-core-nuxqa6/actions/workflows/slack-trigger.yml/dispatches",
-      {
-        method: "POST",
-        headers: {
-          "Accept": "application/vnd.github+json",
-          "Authorization": `Bearer ${githubToken}`,
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-        body: JSON.stringify({
-          ref: "feat/ImplementacionSlack",
-          inputs: { mensaje },
-        }),
-      }
-    );
-
-    if (!response.ok) {
-      const error = await response.json();
-      return res.status(response.status).json(error);
+    console.log("TOKEN cargado:", !!process.env.GITHUB_TOKEN);
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed" });
     }
 
-    res.status(200).json({ message: "Workflow triggered successfully" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+    const { mensaje } = req.body;
+    const githubToken = process.env.GITHUB_TOKEN;
+
+    try {
+
+        const response = await fetch(
+            "https://api.github.com/repos/Aviancaswat/avianca-test-core-nuxqa6/actions/workflows/slack-trigger.yml/dispatches",
+            {
+                method: "POST",
+                headers: {
+                    "Accept": "application/vnd.github+json",
+                    "Authorization": `Bearer ${githubToken}`,
+                    "X-GitHub-Api-Version": "2022-11-28",
+                },
+                body: JSON.stringify({
+                    ref: "feat/ImplementacionSlack",
+                    inputs: { mensaje },
+                }),
+            }
+        );
+
+        if (!response.ok) {
+            const error = await response.json();
+            return res.status(response.status).json(error);
+        }
+
+        res.status(200).json({ message: "Workflow triggered successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }
