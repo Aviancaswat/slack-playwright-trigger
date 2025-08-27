51b3ee8e6aa5eccb3252e20ae92685951ecae2e6
Aviancaswat <lauritablanco2495@gmail.com>
2025-08-27 15:10:59 -0500

    se prueba que llegue texto de slack

--- api/slack.js
+++ api/slack.js
@@ -1,6 +1,5 @@
 // /api/slack.js
 export default async function handler(req, res) {
-    console.log("TOKEN cargado:", !!process.env.GITHUB_TOKEN);
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Method not allowed" });
     }
@@ -21,7 +20,7 @@ export default async function handler(req, res) {
                 },
                 body: JSON.stringify({
                     ref: "feat/ImplementacionSlack",
-                    inputs: { mensaje },
+                    inputs: { mensaje: text || "Sin mensaje" },
                 }),
             }
         );
@@ -31,7 +30,7 @@ export default async function handler(req, res) {
             return res.status(response.status).json(error);
         }
 
-        res.status(200).json({ message: "Workflow triggered successfully" + mensaje });
+        res.status(200).json({ message: `Workflow triggered con mensaje: ${mensaje}`});
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
